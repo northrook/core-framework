72f9db2090ad307a7b6c819dd498fc8470af51d3
Martin <hello@northrook.com>
2024-11-01 08:14:55 +0000

    mend

--- src/DependencyInjection/RegisterCoreServicesPass.php
+++ src/DependencyInjection/RegisterCoreServicesPass.php
@@ -5,21 +5,47 @@ declare(strict_types=1);
 namespace Core\Framework\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\{ContainerBuilder, Reference};
 use function Support\uses_trait;
+use CompileError;
 
 final class RegisterCoreServicesPass implements CompilerPassInterface
 {
     public function process( ContainerBuilder $container ) : void
     {
-        dump( $container->findTaggedServiceIds( 'core.service_container' ) );
+        if ( ! $container->hasDefinition( 'core.service_locator' ) ) {
+            return;
+        }
 
-        $coreServiceLocator = $container->getDefinition( 'core.service_locator' );
+        $this->registerTaggedServices( $container );
+        $this->injectServiceLocator( $container );
+    }
 
+    private function registerTaggedServices( ContainerBuilder $container ) : void
+    {
+
+        $serviceLocatorArguments = $container->getDefinition( 'core.service_locator' )->getArguments()[0] ?? [];
+
+        foreach ( $container->findTaggedServiceIds( 'core.service_locator' ) as $id => $unused ) {
+            $taggedService = $container->getDefinition( $id );
+            $serviceId     = $taggedService->innerServiceId ?? $taggedService->getClass();
+            if ( ! $serviceId ) {
+                throw new CompileError( $this::class." could not find a serviceId for '{$id}' when parsing services tagged with 'core.service_locator'." );
+            }
+            $serviceLocatorArguments[$id] = new Reference( $serviceId );
+        }
+
+        $container->getDefinition( 'core.service_locator' )->setArguments( [$serviceLocatorArguments] );
+    }
+
+
+    private function injectServiceLocator( ContainerBuilder $container ) : void
+    {
         foreach ( \get_declared_classes() as $class ) {
             if ( uses_trait( $class, ServiceContainer::class ) && $container->hasDefinition( $class ) ) {
-                $container->getDefinition( $class )->addMethodCall( 'setServiceLocator', [$coreServiceLocator] );
+                $container->getDefinition( $class )->addMethodCall( 'setServiceLocator', [$container->getDefinition( 'core.service_locator' )] );
             }
         }
     }
+
 }
--- src/DependencyInjection/ServiceContainer.php
+++ src/DependencyInjection/ServiceContainer.php
@@ -4,37 +4,15 @@ declare(strict_types=1);
 
 namespace Core\Framework\DependencyInjection;
 
-use Core\Framework\Response\{Document, Headers, Parameters};
-use Core\Service\{Pathfinder, Request};
 use Symfony\Component\DependencyInjection\ServiceLocator;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Service\Attribute\Required;
 
 /**
- * @property-read Request               $request
- * @property-read Pathfinder            $pathfinder
- * @property-read Document              $document
- * @property-read Parameters            $parameters
- * @property-read Headers               $headers
- *
  * @author Martin Nielsen <mn@northrook.com>
  */
 trait ServiceContainer
 {
     protected readonly ServiceLocator $serviceLocator;
-
-    public function __get( string $service )
-    {
-        return match ( $service ) {
-            'request'        => $this->serviceLocator( Request::class ),
-            'pathfinder'     => $this->serviceLocator( Pathfinder::class ),
-            'document'       => $this->serviceLocator( Document::class ),
-            'parameters'     => $this->serviceLocator( Parameters::class ),
-            'headers'        => $this->serviceLocator( Headers::class ),
-            'serviceLocator' => $this->serviceLocator( ServiceLocator::class ),
-        };
-    }
 
     #[Required]
     final public function setServiceLocator( ServiceLocator $serviceLocator ) : void
@@ -43,6 +21,8 @@ trait ServiceContainer
     }
 
     /**
+     * @final
+     *
      * @template Service
      *
      * @param class-string<Service> $get
