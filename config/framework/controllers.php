c85fe44e3fcaf7490847817f61967e2ae0c532a8
Martin <hello@northrook.com>
2025-02-04 17:24:58 +0000

    mend

---
+++ config/framework/controllers.php
@@ -0,0 +1,30 @@
+<?php
+
+// -------------------------------------------------------------------
+// config\framework\controllers
+// -------------------------------------------------------------------
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Core\Controller\{PublicController, SecurityController};
+
+return static function( ContainerConfigurator $controller ) : void {
+    $framework = $controller->services()
+        ->defaults()
+        ->tag( 'controller.service_arguments' )
+        ->tag( 'monolog.logger', ['channel' => 'request'] );
+
+    $framework->set( SecurityController::class );
+
+    // $framework->set( FaviconController::class );
+
+    $framework
+        ->set( PublicController::class )
+        ->autowire();
+
+    // $framework
+    //     ->set( AdminController::class )
+    //     ->autowire();
+};
