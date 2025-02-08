1ae2bdaecd428b54756845a9fa768cdc77400ef7
Martin <hello@northrook.com>
2025-02-08 12:49:04 +0000

    mend

--- config/view.php
+++ config/view.php
@@ -8,10 +8,57 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Core\Interface\IconProviderInterface;
 use Core\Pathfinder;
-use Core\View\{Document, DocumentView, Latte\ViewComponentExtension, Parameters, TemplateEngine};
+use Core\View\{ComponentFactory,
+    ComponentFactory\ComponentBag,
+    Document,
+    DocumentView,
+    IconSet,
+    Latte\ViewComponentExtension,
+    Parameters,
+    TemplateEngine
+};
+use Core\Symfony\DependencyInjection\CompilerPass;
 
 return static function( ContainerConfigurator $container ) : void {
+    // Component Service Locator
+    $container->services()
+        ->set( 'view.component_locator' )
+        ->tag( 'container.service_locator' )
+        ->args( CompilerPass::PLACEHOLDER_ARGS );
+
+    $container->services()
+        ->set( IconSet::class )
+        ->alias( IconProviderInterface::class, IconSet::class );
+
+    $services = $container->services();
+    // ->defaults()
+    // ->autoconfigure();
+
+    // IconSet
+
+    $services
+        ->set( ComponentFactory::class )
+        ->args(
+            [
+                service( 'view.component_locator' ),
+                abstract_arg( ComponentBag::class ),
+                abstract_arg( 'ComponentProperties::tagged' ),
+                service( 'logger' )->nullOnInvalid(),
+            ],
+        )
+        ->private(); // ->lazy()
+
+    $services
+        ->set( ViewComponentExtension::class )
+        ->args(
+            [
+                service( ComponentFactory::class ),
+                service( 'logger' )->nullOnInvalid(),
+            ],
+        );
+
     $container->services()
             //
         ->set( TemplateEngine::class )
