3212ec20f18ca6cd2de7fa1d3ff40c57c488952c
Martin <hello@northrook.com>
2025-02-19 06:46:23 +0000

    mend

--- config/pathfinder.php
+++ config/pathfinder.php
@@ -8,24 +8,18 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Cache\{LocalStorage, LocalStoragePool};
-use Core\{Pathfinder, Interface\PathfinderInterface};
+use Cache\{LocalStoragePool};
+use Core\{Pathfinder, Interface\PathfinderInterface, Symfony\Cache\LocalStorageAdapter};
 
 return static function( ContainerConfigurator $container ) : void {
     // Cache
-    $container->services()
-        ->set( 'core.pathfinder_cache', LocalStorage::class )
-        ->tag( 'monolog.logger', ['channel' => 'pathfinder'] )
-        ->args(
-            [
-                '%kernel.cache_dir%/pathfinder.cache', // $filePath
-                'pathfinder.cache', // $name
-                Pathfinder::class, // $generator
-            ],
-        );
+    // $container->services()
+    //           ->set( 'cache.pathfinder', LocalStoragePool::class )
+    //           ->tag( 'cache.pool' )
+    //           ->args( ['%kernel.cache_dir%/pathfinder_cache.php'] );
 
     $container->services()
-        ->set( 'cache.pathfinder', LocalStoragePool::class )
+        ->set( 'cache.pathfinder', LocalStorageAdapter::class )
         ->tag( 'cache.pool' )
         ->args( ['%kernel.cache_dir%/pathfinder_cache.php'] );
 
