24a338a0749d03d4c386b3412fd76fd31855236b
Martin <hello@northrook.com>
2025-02-24 13:37:19 +0000

    mend

--- src/Framework/Assets/CoreScript.php
+++ src/Framework/Assets/CoreScript.php
@@ -6,23 +6,13 @@ use Core\Asset\Script;
 use Core\AssetManager\Compiler\AssetService;
 use InvalidArgumentException;
 use Core\AssetManager\Interface\{AssetInterface, AssetServiceInterface};
-use Psr\Log\{LoggerAwareInterface, LoggerInterface};
-use Symfony\Contracts\Service\Attribute\Required;
 
 /**
  * @internal
  */
 #[AssetService( 'script.core' )]
-final readonly class CoreScript implements AssetServiceInterface, LoggerAwareInterface
+final readonly class CoreScript implements AssetServiceInterface
 {
-    private ?LoggerInterface $logger;
-
-    #[Required]
-    public function setLogger( ?LoggerInterface $logger ) : void
-    {
-        $this->logger = $logger;
-    }
-
     public function __invoke( AssetInterface $asset ) : AssetInterface
     {
         if ( ! $asset instanceof Script ) {
@@ -30,8 +20,6 @@ final readonly class CoreScript implements AssetServiceInterface, LoggerAwareInt
         }
 
         $asset->compile( true );
-
-        $this->logger->info( $asset->minifier->getReport()->string );
 
         return $asset;
     }
