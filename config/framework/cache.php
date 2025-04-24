16fff473604c11c408cafe5aada174bbd438136d
Martin <hello@northrook.com>
2025-04-24 10:41:07 +0100

    mend

--- config/framework/cache.php
+++ config/framework/cache.php
@@ -8,21 +8,31 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Cache\LocalStorage;
 use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 use const Support\CACHE_FOREVER;
 use const Time\HOUR_4;
 
 return static function( ContainerConfigurator $container ) : void {
-    $directory = param( 'kernel.cache_dir' );
-    $cachePool = $container->services()
+    $cache     = $container->services();
+    $cachePool = $cache
         ->defaults()
         ->tag( 'cache.pool' );
 
     $cachePool
         ->set( 'cache.asset_pool', PhpFilesAdapter::class )
-        ->args( ['asset_pool', CACHE_FOREVER, $directory, true] );
+        ->args( ['asset_pool', CACHE_FOREVER, '%kernel.cache_dir%', true] );
 
     $cachePool
         ->set( 'cache.component_pool', PhpFilesAdapter::class )
-        ->args( ['component_pool', HOUR_4, $directory, true] );
+        ->args( ['component_pool', HOUR_4, '%kernel.cache_dir%', true] );
+
+    $cache
+        ->set( 'cache.pathfinder', LocalStorage::class )
+        ->call(
+            'setStopwatch',
+            [service( 'debug.stopwatch' )->ignoreOnInvalid()],
+        )
+        ->tag( 'monolog.logger', ['channel' => 'cache'] )
+        ->args( ['%kernel.cache_dir%/pathfinder_cache.php'] );
 };
--- config/pathfinder.php
+++ config/pathfinder.php
@@ -13,16 +13,6 @@ use Core\Pathfinder;
 use const Support\PLACEHOLDER_ARRAY;
 
 return static function( ContainerConfigurator $container ) : void {
-    // Cache
-    $container->services()
-        ->set( 'cache.pathfinder', LocalStorage::class )
-        ->call(
-            'setStopwatch',
-            [service( 'debug.stopwatch' )->ignoreOnInvalid()],
-        )
-        ->tag( 'monolog.logger', ['channel' => 'cache'] )
-        ->args( ['%kernel.cache_dir%/pathfinder_cache.php'] );
-
     // Pathfinder - Find and return registered paths
     $container->services()
         ->set( Pathfinder::class )
